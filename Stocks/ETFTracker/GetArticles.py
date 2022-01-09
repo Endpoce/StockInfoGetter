4428176098e23ec59d9b8c947d4b12b25cc2f9f3
Endpoce <Aidan.Murphy14@gmail.com>
2022-01-09 02:12:13 -0500

    Added GetArticles.py and functionality

---
+++ Stocks/ETFTracker/GetArticles.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import pandas_datareader as pdr
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import json
+
+symbol = input("Ticker: ").upper()
+
+url = ("https://www.google.com/finance/quote/" + symbol + ":NASDAQ")
+
+# r = requests.get(url)
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'html.parser')
+
+
+links = []
+for link in soup.findAll('a'):
+    links.append(link.get('href'))
+
+for link in links:
+    if "https:" in str(link):
+        print(link)
+    else:
+        pass
